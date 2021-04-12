74f8da919a98676df96db47677f2e7b90b720b81
gigpascua <81612931+gigpascua@users.noreply.github.com>
2021-04-12 20:33:07 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this text file is made for Programming Assignment 2 
+## This will show how Lexical Scoping works 
+## First, it will create a matrix that has get & set functions
+## it will just print the cache from repeatedly printing the inverse matrix 
+## it will prevent the program from looping 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  
+  # will print NULL if mat is not a matrix
+  inv <- NULL
+  
+  #set for matrix
+  set <- function(rix) {
+    mat <<- rix
+    inv <<- NULL
+  }
+  
+  #get for matrix, set for inv, then get for inv
+  get <- function() mat
+  set_inverse <- function(set_inv) inv <<- set_inv
+  get_inverse <- function() inv
+  
+  #the program prints the list of set & get functions
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
+## cacheSolve will proceed on solving the inverse of matrix from input
+## Ifelse function is used
+## If already calculated, else print cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cd_matrix, ...) {
+  inv <- cd_matrix$get_inverse()
+  if(!is.null(inv)) {
+    message("printing inverse of cache matrix")
+    return(inv)
+  }
+  rfm_data <- cd_matrix$get()
+  inv <- solve(rfm_data, ...)
+  cd_matrix$set_inverse(inv)
+  inv
 }
